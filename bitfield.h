1bee18ecbaed9d8c23561108aa6d5081434d91ca
Ben Gamari <bgamari.foss@gmail.com>
2010-01-03 14:41:49 -0500

    Get rid of dumb BIT_SIZEOF macro

--- bitfield.h
+++ bitfield.h
@@ -12,16 +12,14 @@
 
 #pragma once
 
-#define BIT_SIZEOF(type) (8*sizeof(type))
-
 template<typename TWord>
 void set_bits(TWord* data, int start_bit, int length=1)
 {
 	int bit=0, word=0;
 	for (int i=0; i<length; i++) {
 		data[word] |= (1 << bit);
-		word += (bit == BIT_SIZEOF(TWord));
-		bit = (bit == BIT_SIZEOF(TWord)) ? 0 : bit+1;
+		word += (bit == 8*sizeof(TWord));
+		bit = (bit == 8*sizeof(TWord)) ? 0 : bit+1;
 	}
 }
 
@@ -34,8 +32,8 @@ void clear_bits(TWord* data, int start_bit, int length=1)
 	int bit=0, word=0;
 	for (int i=0; i<length; i++) {
 		data[word] &= ~(1 << bit);
-		word += (bit == BIT_SIZEOF(TWord));
-		bit = (bit == BIT_SIZEOF(TWord)) ? 0 : bit+1;
+		word += (bit == 8*sizeof(TWord));
+		bit = (bit == 8*sizeof(TWord)) ? 0 : bit+1;
 	}
 }
 
@@ -53,19 +51,19 @@ void assign_bits(const TSrc* src, TDest* dest, int dest_start_bit, int dest_end_
 {
 	int src_word = 0;
 	int src_bit = 0;
-	int dest_word = dest_start_bit / BIT_SIZEOF(TDest);
-	int dest_bit = dest_start_bit % BIT_SIZEOF(TDest);
+	int dest_word = dest_start_bit / (8*sizeof(TDest));
+	int dest_bit = dest_start_bit % (8*sizeof(TDest));
 
 	int bits = dest_end_bit - dest_start_bit;
 	for (int i=0; i<bits; i++) {
 		dest[dest_word] &= ~(1 << dest_bit);
 		dest[dest_word] |= ((src[src_word] & (1 << src_bit)) != 0);
 
-		src_word += (src_bit == BIT_SIZEOF(TSrc));
-		src_bit = (src_bit == BIT_SIZEOF(TSrc)) ? 0 : src_bit+1;
+		src_word += (src_bit == 8*sizeof(TSrc));
+		src_bit = (src_bit == 8*sizeof(TSrc)) ? 0 : src_bit+1;
 
-		dest_word += (dest_bit == BIT_SIZEOF(TDest));
-		dest_bit = (dest_bit == BIT_SIZEOF(TDest)) ? 0 : dest_bit+1;
+		dest_word += (dest_bit == 8*sizeof(TDest));
+		dest_bit = (dest_bit == 8*sizeof(TDest)) ? 0 : dest_bit+1;
 	}
 }
 
@@ -81,8 +79,8 @@ void assign_bits(const TSrc* src, TDest* dest, int dest_start_bit, int dest_end_
 template<typename TSrc, typename TDest>
 void extract_bits(const TSrc* src, int src_start_bit, int src_end_bit, TDest* dest)
 {
-	int src_word = src_start_bit / BIT_SIZEOF(TSrc);
-	int src_bit = src_start_bit % BIT_SIZEOF(TSrc);
+	int src_word = src_start_bit / (8*sizeof(TSrc));
+	int src_bit = src_start_bit % (8*sizeof(TSrc));
 	int dest_word = 0;
 	int dest_bit = 0;
 
@@ -91,11 +89,11 @@ void extract_bits(const TSrc* src, int src_start_bit, int src_end_bit, TDest* de
 		dest[dest_word] &= ~(1 << dest_bit);
 		dest[dest_word] |= ((src[src_word] & (1 << src_bit)) != 0);
 
-		src_word += (src_bit == BIT_SIZEOF(TSrc));
-		src_bit = (src_bit == BIT_SIZEOF(TSrc)) ? 0 : src_bit+1;
+		src_word += (src_bit == 8*sizeof(TSrc));
+		src_bit = (src_bit == 8*sizeof(TSrc)) ? 0 : src_bit+1;
 
-		dest_word += (dest_bit == BIT_SIZEOF(TDest));
-		dest_bit = (dest_bit == BIT_SIZEOF(TDest)) ? 0 : dest_bit+1;
+		dest_word += (dest_bit == 8*sizeof(TDest));
+		dest_bit = (dest_bit == 8*sizeof(TDest)) ? 0 : dest_bit+1;
 	}
 }
 

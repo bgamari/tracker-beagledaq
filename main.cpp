314f552e7fbca21cc7258aa40240af58f29299b4
Ben Gamari <bgamari.foss@gmail.com>
2010-01-04 19:01:55 -0500

    Implement adc and dac glue layers

--- main.cpp
+++ main.cpp
@@ -12,6 +12,34 @@ static uint8_t bits = 16;
 static uint32_t speed = 500000;
 
 
+struct max1270_inputs {
+	max1270& adc;
+	std::vector<int> channels;
+
+	max1270_inputs(max1270& adc, std::vector<int> channels) :
+		adc(adc), channels(channels) { }
+
+	std::vector<uint16_t> get() {
+		adc.get(channels);
+	}
+};
+
+struct max5590_outputs {
+	max5590& dac;
+	std::vector<int> channels;
+
+	max5590_outputs(max5590& dac) : dac(dac), channels(channels) { }
+
+	void set(std::vector<uint16_t> values) {
+		std::vector<std::pair<int, uint16_t> > v;
+		for (int i=0; i<channels.size(); i++) {
+			std::pair<int, uint16_t> p(channels[i], values[i]);
+			v.push_back(p);
+		}
+		dac.set(v);
+	}
+};
+
 int main(int argc, char** argv)
 {
 }

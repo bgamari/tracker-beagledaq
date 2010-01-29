bba89e59722b7120482de446b03d8d42bb5778cb
Ben Gamari <bgamari.foss@gmail.com>
2010-01-28 21:15:29 -0500

    Work on bitfield class

--- bitfield.h
+++ bitfield.h
@@ -18,12 +18,14 @@
  */
 
 #pragma once
+#include <algorithm>
+#include <boost/format.hpp>
 
 /*
  * set_bits(): Sets a range of bits
  */
 template<typename TWord>
-void set_bits(TWord* data, int start_bit, int length=1)
+void set_bits(TWord* data, unsigned int start_bit, unsigned int length=1)
 {
         int word = start_bit / (8*sizeof(TWord));
 	int bit = start_bit % (8*sizeof(TWord));
@@ -38,7 +40,7 @@ void set_bits(TWord* data, int start_bit, int length=1)
  * clear(): Clears a range of bits
  */
 template<typename TWord>
-void clear_bits(TWord* data, int start_bit, int length=1)
+void clear_bits(TWord* data, unsigned int start_bit, unsigned int length=1)
 {
         int word = start_bit / (8*sizeof(TWord));
 	int bit = start_bit % (8*sizeof(TWord));
@@ -59,7 +61,7 @@ void clear_bits(TWord* data, int start_bit, int length=1)
  *
  */
 template<typename TSrc, typename TDest>
-void assign_bits(const TSrc* src, TDest* dest, int dest_start_bit, int dest_end_bit)
+void assign_bits(const TSrc* src, TDest* dest, unsigned int dest_start_bit, unsigned int dest_end_bit)
 {
 	int src_word = 0;
 	int src_bit = 0;
@@ -90,7 +92,7 @@ void assign_bits(const TSrc* src, TDest* dest, int dest_start_bit, int dest_end_
  *
  */
 template<typename TSrc, typename TDest>
-void extract_bits(const TSrc* src, int src_start_bit, int src_end_bit, TDest* dest)
+void extract_bits(const TSrc* src, unsigned int src_start_bit, unsigned int src_end_bit, TDest* dest)
 {
 	int src_word = src_start_bit / (8*sizeof(TSrc));
 	int src_bit = src_start_bit % (8*sizeof(TSrc));
@@ -110,4 +112,164 @@ void extract_bits(const TSrc* src, int src_start_bit, int src_end_bit, TDest* de
 		dest_bit = (dest_bit == 8*sizeof(TDest)-1) ? 0 : dest_bit+1;
 	}
 }
+
+template <typename word_t>
+class generic_bitfield {
+	unsigned int bits;
+	word_t* data;
+
+public:
+	generic_bitfield(unsigned int bits) : bits(bits), data(new word_t[words()]) { }
+
+	template<typename T>
+	generic_bitfield(unsigned int bits, T d) : bits(bits), data(new word_t[words()]) {
+		*((T*) data) = d;
+		unsigned int i = bits/sizeof(word_t) + (bits%sizeof(word_t) != 0);
+		for (; i<words(); i++)
+			data[i] = 0;
+	}
+
+	unsigned int words() const {
+		 return bits/sizeof(word_t) + (bits%sizeof(word_t) != 0);
+	}
+
+	unsigned int size() const {
+		return bits;
+	}
+
+	bool get(unsigned int bit) const {
+		word_t& d = data[bit/sizeof(word_t)];
+		word_t mask = 1 << (bit%sizeof(word_t));
+		return (d & mask) != 0;
+	}
+
+	void set(unsigned int bit, bool value) {
+		word_t& d = data[bit/sizeof(word_t)];
+		word_t mask = 1 << (bit%sizeof(word_t));
+		if (value)
+			d |= mask;
+		else
+			d &= ~mask;
+	}
+
+	generic_bitfield operator<<(unsigned int shift) const {
+		generic_bitfield res(bits, 0);
+		for (unsigned int i=shift; i<bits; i++)
+			res.set(i, get(i-shift));
+		return res;
+	}
+
+	generic_bitfield operator>>(unsigned int shift) const {
+		generic_bitfield res(bits, 0);
+		for (unsigned int i=0; i<bits-shift; i++)
+			res.set(i, get(i+shift));
+		return res;
+	}
+
+	generic_bitfield operator|(const generic_bitfield& bf) const {
+		generic_bitfield res(bits);
+		unsigned int b = std::min(words(), bf.words());
+		for (unsigned int i=0; i<b; i++)
+			res.data[i] = bf.data[i] | data[i];
+		return res;
+	}
+
+	generic_bitfield operator&(const generic_bitfield& bf) const {
+		generic_bitfield res(bits);
+		unsigned int b = std::min(words(), bf.words());
+		for (unsigned int i=0; i<b; i++)
+			res.data[i] = bf.data[i] & data[i];
+		return res;
+	}
+
+	generic_bitfield& operator|=(const generic_bitfield& bf) {
+		unsigned int b = std::min(words(), bf.words());
+		for (unsigned int i=0; i<b; i++)
+			data[i] |= bf.data[i];
+		return *this;
+	}
+
+	generic_bitfield& operator&=(const generic_bitfield& bf) {
+		unsigned int b = std::min(words(), bf.words());
+		for (unsigned int i=0; i<b; i++)
+			data[i] &= bf.data[i];
+		return *this;
+	}
+
+	bool operator==(const generic_bitfield& b) const {
+		if (bits != b.bits)
+			return false;
+		for (unsigned int i=0; i<words(); i++)
+			if (data[i] != b.data[i]) return false;
+		return true;
+	}
+
+	bool operator!=(const generic_bitfield& b) const {
+		return !(*this == b);
+	}
+
+	generic_bitfield resized(unsigned int size) const {
+		generic_bitfield res(size);
+		int mask_size = std::min(bits, size);
+		for (int i=0; i < res.words(); i++) {
+			uint8_t mask = 0xff;
+			if (mask_size-sizeof(word_t)*i < sizeof(word_t))
+				mask = (1 << (mask_size - 8*i)) - 1;
+			res.data[i] = data[i] & mask;
+		}
+		return res;
+	}
+
+	generic_bitfield concat(const generic_bitfield& b) const {
+		generic_bitfield res = resized(bits + b.bits);
+		generic_bitfield c = b.resized(bits + b.bits) << bits;
+		res |= c;
+		return res;
+	}
+
+	class reference {
+		friend class generic_bitfield;
+		generic_bitfield& bf;
+		const unsigned int bit;
+	private:
+		reference(generic_bitfield& bf, unsigned int bit) : bf(bf), bit(bit) { }
+	public:
+		reference& operator=(bool value) {
+			bf.set(bit, value);
+			return *this;
+		}
+		reference& operator=(const reference& ref) {
+			bool value = ref.bf.get(ref.bit);
+			bf.set(bit, value);
+			return *this;
+		}
+		bool operator~() const {
+			return !(*this);
+		}
+		operator bool() const {
+			return bf.get(bit);
+		}
+		reference& flip() {
+			bool value = *this;
+			bf.set(bit, !value);
+		}
+	};
+	friend class reference;
+
+	reference operator[](unsigned int bit) {
+		return reference(*this, bit);
+	};
+
+	std::string str() {
+		std::string s;
+		s.reserve(3*words() + 1);
+		boost::format fmt("%02x ");
+		for (unsigned int i=0; i<words(); i++)
+			s.append((fmt % data[words()-i-1]).str());
+		s.append("\n");
+		return s;
+	}
+};
+
+typedef generic_bitfield<unsigned int> bitfield;
 
--- test_bitfield.cpp
+++ test_bitfield.cpp
@@ -10,6 +10,8 @@
 #include <cstdint>
 #include <cstdio>
 #include <cstring>
+#include <iostream>
+#include <endian.h>
 #include "bitfield.h"
 
 #define FAIL(test) fprintf(stderr, "Failed "test" test\n");
@@ -43,9 +45,26 @@ void test_extract_bits() {
 	//printf("%08x\n", b);
 }
 
+void test_bitfield() {
+	bitfield a(32, 0x12345678);
+	bitfield b(32, 0xaaaabbbb);
+	
+	std::cout << (a >> 14).str();
+	if ((a >> 14) != bitfield(32, 0x000048d1)) FAIL("bitfield >>");
+	if ((a << 14) != bitfield(32, 0x159e0000)) FAIL("bitfield <<");
+	
+	if ((a & b) != bitfield(32, 0x02201238)) FAIL("bitfield AND");
+	if ((a | b) != bitfield(32, 0xbabefffb)) FAIL("bitfield OR");
+	
+	if (a.resized(40) != bitfield(40, 0x0012345678ULL)) FAIL("bitfield resize");
+	if (a.concat(b) != bitfield(64, 0xaaaabbbb12345678ULL)) FAIL("bitfield concat");
+}
+
 int main() {
 	test_set_clear_bits();
 	test_assign_bits();
 	test_extract_bits();
+
+	test_bitfield();
 }
 

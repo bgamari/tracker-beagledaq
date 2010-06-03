a5e40114291b40baef3365b93bd59405c57fc64d
Ben Gamari <bgamari.foss@gmail.com>
2010-06-03 09:45:54 -0400

    Split out test code into separate function

--- main.cpp
+++ main.cpp
@@ -32,6 +32,33 @@
 using std::array;
 using std::string;
 
+void dump_data_test(input_channels<4>& psd_inputs, input_channels<1>& pd_input,
+		stage& stage, input_channels<3>& fb_inputs)
+{
+	printf("# psd_x psd_y sum_x sum_y\tfb_x fb_y fb_z\tpd\n");
+	unsigned int n=0;
+	while (true) {
+		Vector4f psd = psd_inputs.get();
+#define SCALE_INPUTS 1
+#if SCALE_INPUTS
+                psd.x() /= psd[2];
+                psd.y() /= psd[3];
+#endif
+		Vector3f fb = fb_inputs.get();
+                Matrix<float,1,1> pd = pd_input.get();
+		for (int i=0; i<4; i++) printf("%f ", psd[i]);
+		printf("\t");
+		for (int i=0; i<3; i++) printf("%f ", fb[i]);
+		printf("\t%f\n", pd[0]);
+
+		usleep(1000*10);
+		n++;
+                Vector3f pos;
+                pos << 0.5 + 0.2*sin(0.01*n), 0.5, 0.5;
+                //stage.move(pos);
+	}
+}
+
 int main(int argc, char** argv)
 {
 //#define TEST
@@ -87,38 +114,11 @@ int main(int argc, char** argv)
 			std::cout << "Invalid command\n";
 	}
 
-#define TRACK
-#ifdef TRACK
 	fprintf(stderr, "Position bead. Press any key.\n");
 	getchar();
 	Vector4f psd = psd_inputs.get();
 	fprintf(stderr, "PSD: pos=(%f, %f); sum=(%f, %f)\n",
 			psd[0], psd[1], psd[2], psd[3]);
-	track(psd_inputs, stage, fb_inputs);
-#else
-	printf("# psd_x psd_y sum_x sum_y\tfb_x fb_y fb_z\tpd\n");
-	unsigned int n=0;
-	while (true) {
-		Vector4f psd = psd_inputs.get();
-#define SCALE_INPUTS 1
-#if SCALE_INPUTS
-                psd.x() /= psd[2];
-                psd.y() /= psd[3];
-#endif
-		Vector3f fb = fb_inputs.get();
-                Matrix<float,1,1> pd = pd_input.get();
-		for (int i=0; i<4; i++) printf("%f ", psd[i]);
-		printf("\t");
-		for (int i=0; i<3; i++) printf("%f ", fb[i]);
-		printf("\t%f\n", pd[0]);
-
-		usleep(1000*10);
-		n++;
-                Vector3f pos;
-                pos << 0.5 + 0.2*sin(0.01*n), 0.5, 0.5;
-                //stage.move(pos);
-	}
-#endif
 	tracker.track();
 	return 0;
 }

e151df48a3c3ffdbf5aaaa2c8c4c4b02dfc558c0
Ben Gamari <bgamari.foss@gmail.com>
2011-05-16 15:48:56 -0400

    Move tracker hardware glue code into hardware/tracker.h

--- channels.h
+++ channels.h
@@ -23,7 +23,6 @@
 #pragma once
 #include <array>
 #include <Eigen/Eigen>
-#include "hardware/tracker.h"
 
 using namespace Eigen;
 using std::array;
@@ -62,86 +61,4 @@ struct test_outputs : output_channels<N> {
 	}
 };
 
-template<unsigned int N>
-struct max1302_inputs : input_channels<N> {
-	max1302& dev;
-	array<int,N> channels;
-	// Normalization parameters:
-	float scale; // Full-scale range, in units of Vref
-	float offset; // Offset, in units of Vref
-
-	max1302_inputs(max1302& dev, const array<int,N> channels,
-			max1302::input_range range) :
-		dev(dev), channels(channels)
-	{
-		std::vector<max1302::command*> cmds;
-		
-		switch (range) {
-		case max1302::SE_ZERO_PLUS_VREF:
-			offset = 0; scale = 1; break;
-		case max1302::SE_MINUS_VREF_PLUS_VREF:
-			offset = -1; scale = 2; break;
-		case max1302::SE_ZERO_PLUS_VREF2:
-			offset = 0; scale = 0.5; break;
-		default:
-			fprintf(stderr, "Unknown range\n");
-			abort();
-		}
-		
-		for (unsigned int i=0; i<N; i++) {
-			cmds.push_back(new max1302::mode_control_cmd(max1302::EXT_CLOCK));
-			cmds.push_back(new max1302::input_config_cmd(channels[i], range));
-		}
-		dev.submit(cmds);
-		for (auto c=cmds.begin(); c != cmds.end(); c++)
-			delete *c;
-	}
-
-	Matrix<float,1,N> get() const
-	{
-		array<uint16_t,N> int_vals;
-		Matrix<float,1,N> values;
-		std::vector<max1302::command*> cmds;
-		cmds.reserve(N);
-
-		for (unsigned int i=0; i<N; i++)
-			cmds.push_back(new max1302::start_conversion_cmd(channels[i], &int_vals[i]));
-		dev.submit(cmds);
-		for (unsigned int i=0; i<N; i++) {
-			int_vals[i] *= 2; // HACK: Hardware seems to be off by factor of two
-			values[i] = scale * int_vals[i] / 0xffff + offset;
-			delete cmds[i];
-		}
-		return values;
-	}
-};
-
-template<unsigned int N>
-struct max5134_outputs : output_channels<N> {
-	max5134& dev;
-	array<max5134::chan_mask,N> channels;
-
-	max5134_outputs(max5134& dev, const array<max5134::chan_mask,N> channels) :
-		dev(dev), channels(channels) { }
-
-	void set(const Matrix<float,1,N> values) const
-	{
-		max5134::chan_mask all_mask;
-		std::vector<max5134::command*> cmds;
-		cmds.reserve(N+1);
-		for (unsigned int i=0; i<N; i++) {
-			if (values[i] < 0.0 || values[i] > 1.0)
-				throw clamped_output_error(values);
-			uint16_t out_val = lrint(values[i]*0xffff);
-			cmds.push_back(new max5134::write_cmd(channels[i], out_val));
-			all_mask |= channels[i];
-		}
-		cmds.push_back(new max5134::load_dac_cmd (all_mask));
-
-		dev.submit(cmds);
-		for (auto c=cmds.begin(); c != cmds.end(); c++)
-			delete *c;
-	}
-};
-
-
+#include "hardware/tracker.h"
--- hardware/tracker.h
+++ hardware/tracker.h
@@ -27,6 +27,7 @@
 #include <cstring>
 #include <bitset>
 
+#include "channels.h"
 #include "spi_device.h"
 
 class max1302 : spi_device
@@ -108,6 +109,60 @@ public:
 
 	void submit(std::vector<command*>& cmds) {
 		spi_device::submit(cmds);
+	}
+};
+
+template<unsigned int N>
+struct max1302_inputs : input_channels<N> {
+	max1302& dev;
+	array<int,N> channels;
+	// Normalization parameters:
+	float scale; // Full-scale range, in units of Vref
+	float offset; // Offset, in units of Vref
+
+	max1302_inputs(max1302& dev, const array<int,N> channels,
+			max1302::input_range range) :
+		dev(dev), channels(channels)
+	{
+		std::vector<max1302::command*> cmds;
+		
+		switch (range) {
+		case max1302::SE_ZERO_PLUS_VREF:
+			offset = 0; scale = 1; break;
+		case max1302::SE_MINUS_VREF_PLUS_VREF:
+			offset = -1; scale = 2; break;
+		case max1302::SE_ZERO_PLUS_VREF2:
+			offset = 0; scale = 0.5; break;
+		default:
+			fprintf(stderr, "Unknown range\n");
+			abort();
+		}
+		
+		for (unsigned int i=0; i<N; i++) {
+			cmds.push_back(new max1302::mode_control_cmd(max1302::EXT_CLOCK));
+			cmds.push_back(new max1302::input_config_cmd(channels[i], range));
+		}
+		dev.submit(cmds);
+		for (auto c=cmds.begin(); c != cmds.end(); c++)
+			delete *c;
+	}
+
+	Matrix<float,1,N> get() const
+	{
+		array<uint16_t,N> int_vals;
+		Matrix<float,1,N> values;
+		std::vector<max1302::command*> cmds;
+		cmds.reserve(N);
+
+		for (unsigned int i=0; i<N; i++)
+			cmds.push_back(new max1302::start_conversion_cmd(channels[i], &int_vals[i]));
+		dev.submit(cmds);
+		for (unsigned int i=0; i<N; i++) {
+			int_vals[i] *= 2; // HACK: Hardware seems to be off by factor of two
+			values[i] = scale * int_vals[i] / 0xffff + offset;
+			delete cmds[i];
+		}
+		return values;
 	}
 };
 
@@ -205,6 +260,34 @@ public:
 
 	void submit(std::vector<command*>& cmds) {
 		spi_device::submit(cmds);
+	}
+};
+
+template<unsigned int N>
+struct max5134_outputs : output_channels<N> {
+	max5134& dev;
+	array<max5134::chan_mask,N> channels;
+
+	max5134_outputs(max5134& dev, const array<max5134::chan_mask,N> channels) :
+		dev(dev), channels(channels) { }
+
+	void set(const Matrix<float,1,N> values) const
+	{
+		max5134::chan_mask all_mask;
+		std::vector<max5134::command*> cmds;
+		cmds.reserve(N+1);
+		for (unsigned int i=0; i<N; i++) {
+			if (values[i] < 0.0 || values[i] > 1.0)
+				throw clamped_output_error(values);
+			uint16_t out_val = lrint(values[i]*0xffff);
+			cmds.push_back(new max5134::write_cmd(channels[i], out_val));
+			all_mask |= channels[i];
+		}
+		cmds.push_back(new max5134::load_dac_cmd (all_mask));
+
+		dev.submit(cmds);
+		for (auto c=cmds.begin(); c != cmds.end(); c++)
+			delete *c;
 	}
 };
 
